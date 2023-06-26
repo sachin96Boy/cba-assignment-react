dab572ac5c4bfff469a1e1161e76b973a41340db
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-26 07:59:01 +0530

    chor: min change

--- src/pages/SigninScreen.tsx
+++ src/pages/SigninScreen.tsx
@@ -12,25 +12,6 @@ import { AiOutlineBlock } from "react-icons/ai";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
 export default function SignInScreen() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -43,7 +24,6 @@ export default function SignInScreen() {
 
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
       <Box
         sx={{
           marginTop: 8,
@@ -105,7 +85,6 @@ export default function SignInScreen() {
           </Grid>
         </Box>
       </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
     </Container>
   );
 }
