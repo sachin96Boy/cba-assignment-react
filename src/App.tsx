2566d09251d65e48b1a3fdf9417beae858e35d9f
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-26 07:28:17 +0530

    chor: routing and signinpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import { Route, Routes, Navigate } from "react-router-dom";
+import SignInScreen from "./pages/SigninScreen";
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          React app with Material Theme
-        </Typography>
-      </Box>
-    </Container>
+    <Routes>
+      <Route path="/" element={<SignInScreen />} />
+      <Route path="*" element={<Navigate to={"/"} replace />} />
+    </Routes>
   );
 }
 
