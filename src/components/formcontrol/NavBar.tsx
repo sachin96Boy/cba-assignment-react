38a3024203c7b3adbcf69e0c3ba9484cb85846c8
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-29 09:53:55 +0530

    chor: rem of image src

--- src/components/formcontrol/NavBar.tsx
+++ src/components/formcontrol/NavBar.tsx
@@ -112,7 +112,7 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="User" />
               </IconButton>
             </Tooltip>
             <Menu
