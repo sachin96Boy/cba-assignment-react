f66be0f072dc547f0796758c4bedc58d76d4a42f
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-29 09:25:19 +0530

    init: navbar

--- src/pages/DashboarScreen.tsx
+++ src/pages/DashboarScreen.tsx
@@ -1,5 +1,12 @@
+import { Box } from "@mui/material";
+import ResponsiveAppBar from "../components/formcontrol/NavBar";
+
 function DashboarScreen() {
-  return <div>DashboarScreen</div>;
+  return (
+    <Box height={"100vh"} width={"100%"}>
+      <ResponsiveAppBar />
+    </Box>
+  );
 }
 
 export default DashboarScreen;
