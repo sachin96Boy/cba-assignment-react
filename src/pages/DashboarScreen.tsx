74e09390148edf062b1365158778db8731d61d57
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-30 10:20:06 +0530

    chor: assets added

--- src/pages/DashboarScreen.tsx
+++ src/pages/DashboarScreen.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import ResponsiveAppBar from "../components/formcontrol/NavBar";
+import ResponsiveAppBar from "../components/NavBar";
 
 function DashboarScreen() {
   return (
