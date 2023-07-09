2a5cba5a43947f0c9295e591b15fdb791a2b9d3b
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-07-09 08:11:35 +0530

    chor: page add

--- src/components/Reportlist.tsx
+++ src/components/Reportlist.tsx
@@ -3,6 +3,8 @@ import instance from "../utils/AxiosInstance";
 import { AxiosResponse } from "axios";
 import { Box, Divider, Stack } from "@mui/material";
 import ReportCard from "./Reportcard/ReportCard";
+import LoadingSpinner from "./spinner/LoadingSpinner";
+import Errorcomponent from "./error/Errorcomponent";
 
 function Reportlist() {
   const [reportList, setReportList] = React.useState([]);
@@ -28,9 +30,27 @@ function Reportlist() {
   const showingReports = (
     <>
       {isLoading ? (
-        <Box>loading</Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100vh",
+          }}
+        >
+          <LoadingSpinner />
+        </Box>
       ) : isError ? (
-        <Box>Something went Wrong</Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100vh",
+          }}
+        >
+          <Errorcomponent />
+        </Box>
       ) : (
         <Stack
           direction={"column"}
